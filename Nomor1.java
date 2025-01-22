eeb2324df568803052a56094655f1cace005853f
Sharaanjelia <sharaanjelia236@gmail.com>
2025-01-22 13:58:10 +0700

    Update Nomor1.java

--- Nomor1.java
+++ Nomor1.java
@@ -1,5 +1,14 @@
 public class Nomor1{
      public static void main(String[] args) {
-        
+        public static int hitung biaya taksi () {
+        if (kilometer < 3 kilometer) {
+             return 20000;
+        } else if (kilometer >=3 kilo meter && jarak jamnya >=3 jam){
+             return 20000;
+        } else {
+             return 10000;
+        }
+     }
+             
     }
 }
