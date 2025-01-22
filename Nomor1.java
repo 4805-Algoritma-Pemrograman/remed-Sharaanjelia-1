998d9fa0abb5daede52730a49d8b0fd2420c1555
Sharaanjelia <sharaanjelia236@gmail.com>
2025-01-22 14:06:12 +0700

    Update Nomor1.java

--- Nomor1.java
+++ Nomor1.java
@@ -3,12 +3,15 @@ public class Nomor1{
         public static int hitung biaya taksi () {
         if (kilometer < 3 kilometer) {
              return 20000;
-        } else if (kilometer >=3 kilo meter && jarak jamnya >=3 jam){
+        } else if (kilometer >=3 kilo meter && waktu  >=3 jam + 10000 ){
              return 20000;
         } else {
              return 10000;
         }
      }
-             
+      System.out.println (Masukan jumlah kilometer: ")
+     int total kilometer = scanner.nextint();
+     
+      System.out.println("total kilometer+ waktu) ;    
     }
 }
